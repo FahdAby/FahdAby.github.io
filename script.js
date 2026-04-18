0bd6412d73cb5e403d567ff0b1335c325b1f30dd
FahdAby <fahdhillaby2711@gmail.com>
2026-04-18 18:00:07 +0700

    Initial commit for Data Science Portfolio

---
+++ script.js
@@ -0,0 +1,32 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        // Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+
+        // Burger Animation (buat jadi silang)
+        burger.classList.toggle('toggle');
+    });
+
+    // Menutup menu jika salah satu link di-klik (berguna untuk HP)
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            nav.classList.remove('nav-active');
+            burger.classList.remove('toggle');
+        });
+    });
+}
+
+navSlide();
